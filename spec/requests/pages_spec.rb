fbed43b0cf8c755fa263103418272d40f336caa5
RL <rl@rl-VirtualBox.(none)>
2013-05-27 16:08:22 +0200

    Integrated Title Helper

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -8,5 +8,16 @@ describe "Pages" do
       visit '/pages/home'
       expect(page).to have_content('Willkommen')
     end
+
+    it "should have the base title" do
+      visit '/pages/home'
+      page.should have_selector('title', :text => "ReGen")
+    end
+
+    it "should not have a custom page title" do
+      visit '/pages/home'
+      page.should_not have_selector('title', :text => '| Home')      
+    end
+    
   end
 end
