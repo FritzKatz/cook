a0f95ebf40b3bf45469f5946cf771c4b4d649339
RL <rl@rl-VirtualBox.(none)>
2013-05-27 14:30:55 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cook::Application.config.secret_token = '9cad6c0757afc23c76feb0ce51ca24bec32d8da6cb5e5c9bad6bf6a568a4ca357a81cc53826d8f220467aac4c98d1ce17cdb0dea74b7d9a38e8faa83473c229a'
